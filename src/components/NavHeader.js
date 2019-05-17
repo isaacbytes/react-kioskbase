b3a7e4fa4b9d180d67adf8864833e3a69ebee9d0
Isaac Farciert <rfisaac2296@yahoo.com>
2019-05-17 16:44:18 -0400

    Add support for CSS modules

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 
+import styles from '../styles/components/NavHeader.module.scss';
+
 export default class NavHeader extends Component {
   render() {
     return (
       <div>
         This is the NavHeader component.
-        <header>
+        <header className={styles.navHeader}>
           <nav>
             <ul>
               <li>Home</li>
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,37 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 
 module.exports = function(env, arg) {
   const isProduction = env === 'production';
+
+  const CSSModuleLoader = {
+    loader: 'css-loader',
+    options: {
+      modules: true,
+      sourceMap: true,
+      localIdentName: '[local]__[hash:base64:5]'
+    }
+  };
+  const CSSLoader = {
+    loader: 'css-loader',
+    options: {
+      modules: false,
+      sourceMap: true
+    }
+  };
+  const postCSSLoader = {
+    loader: 'postcss-loader',
+    options: {
+      ident: 'postcss',
+      sourceMap: true,
+      plugins: () => [
+        autoprefixer({
+          browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9']
+        })
+      ]
+    }
+  };
 
   return {
     mode: isProduction ? 'production' : 'development',
@@ -12,11 +41,27 @@ module.exports = function(env, arg) {
       filename: 'bundle.js'
     },
     module: {
-      rules: [{
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      }]
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.scss$/,
+          exclude: /\.module\.scss$/,
+          use: ['style-loader', CSSLoader, postCSSLoader, 'sass-loader']
+        },
+        {
+          test: /\.module\.scss$/,
+          use: [
+            'style-loader',
+            CSSModuleLoader,
+            postCSSLoader,
+            'sass-loader',
+          ]
+        }
+      ]
     },
     devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
