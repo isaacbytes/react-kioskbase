79651dfd6f63a06a1c2d32667aea9f3bd9668c9c
Isaac Farciert <rfisaac2296@yahoo.com>
2019-05-24 16:25:23 -0400

    Set up default application-wide bg image

--- src/components/BackgroundWrap.js
+++ src/components/BackgroundWrap.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import background from '../images/bg.jpg';
 
 export default class BackgroundWrap extends Component {
+
   render() {
+    const bgStyle = {
+      backgroundImage: `url(${background})`,
+      backgroundSize: 'cover',
+      minHeight: '100vh'
+    };
+
     return (
-      <div>
+      <div style={bgStyle}>
         This is the BackgroundWrap component.
 
         {/* We will render the children */}
