6590d2b93392a45101b0c98464f0d8ad22281149
Isaac Farciert <rfisaac2296@yahoo.com>
2019-05-24 16:56:21 -0400

    Fix box-sizing throughout app

--- src/components/BackgroundWrap.js
+++ src/components/BackgroundWrap.js
@@ -7,7 +7,7 @@ export default class BackgroundWrap extends Component {
     const bgStyle = {
       backgroundImage: `url(${background})`,
       backgroundSize: 'cover',
-      minHeight: '100vh',
+      height: '100vh',
       // header height (from component style): 3.2em
       paddingTop: '4em'
     };
