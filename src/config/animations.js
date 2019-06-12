f183989a4de13b3672f219714fa23e2cba1c2d1c
Isaac Farciert <rfisaac2296@yahoo.com>
2019-06-12 19:46:56 -0400

    Abstract away header and footer animations into config file

---
+++ src/config/animations.js
@@ -0,0 +1,17 @@
+// Store all animation-related config here
+
+const HeaderFooterAnimationsConfig = {
+  hidden: {
+    x: '-100%'
+  },
+
+  shown: {
+    x: '0%'
+  }
+}
+
+
+
+export default {
+  HeaderFooterConfig:  {...HeaderFooterAnimationsConfig}
+}
