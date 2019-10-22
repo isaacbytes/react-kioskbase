defd574047e2250d21f0be5b29a7a28ce819cd15
Isaac Farciert <rfisaac2296@yahoo.com>
2019-10-22 17:47:50 -0400

    Adjust Maintenance page layout

--- src/components/pages/maintenance/MaintenanceMain.js
+++ src/components/pages/maintenance/MaintenanceMain.js
@@ -11,27 +11,41 @@ export default class MaintenanceMain extends Component {
         
         <div className="depts-intro">
           <div className="depts-intro__desc">
-            Magna fermentum iaculis eu non diam phasellus vestibulum lorem. 
-            Etiam non quam lacus suspendisse faucibus interdum. Urna neque viverra 
-            justo nec ultrices dui sapien eget. Habitant morbi tristique senectus et netus et. 
-            Lobortis elementum nibh tellus molestie nunc non. Nunc vel risus commodo viverra maecenas 
-            accumsan. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. 
-            Placerat vestibulum lectus mauris ultrices eros in cursus turpis. Pulvinar 
-            sapien et ligula ullamcorper malesuada proin libero nunc. Arcu odio ut sem 
-            nulla pharetra diam sit amet. Felis eget velit aliquet sagittis id consectetur 
-            purus.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+          culpa qui officia deserunt mollit anim id est laborum.
           </div>
 
           <div className="depts-intro__pic">
-            [INSERT PICTURE]
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+          culpa qui officia deserunt mollit anim id est laborum.          
           </div>
         </div>
 
-
-        <div className="depts-preview">
-            [INSERT DEPTS PREVIEW]
+        <div className="depts">
+          <header className="depts-preview-header">Departments Overview</header>
+          <div className="depts-preview">
+            <div className="depts-single">Department Name</div>
+            <div className="depts-single">Department Name</div>
+            <div className="depts-single">Department Name</div>
+            <div className="depts-single">Department Name</div>
+            <div className="depts-single">Department Name</div>
+            <div className="depts-single">Department Name</div>
+            <div className="depts-single">Department Name</div>
+            <div className="depts-single">Department Name</div>
+            <div className="depts-single">Department Name</div>
+            <div className="depts-single">Department Name</div>
+          </div>
         </div>
 
+        <hr />
 
         <div className="join-us">
           <div className="join-us__header">
